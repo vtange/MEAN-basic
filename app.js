568c1e1936088ae9685ce9cc5220d76fc081d2ff
Victor Tang <victortang1992@gmail.com>
2015-10-23 22:13:14 -0700

    hide function, add link

--- app.js
+++ app.js
@@ -10,9 +10,14 @@ app.controller('MainCtrl', ['$scope', function($scope){
       {title: 'post 5', upvotes: 4}
     ];
     $scope.addPost = function(){
-      if(!$scope.title || $scope.title === '') { return; }
-      $scope.posts.push({title: $scope.title, upvotes: 0});
+      if(!$scope.title || $scope.title === '') { return; } // return immediately if empty
+      $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0
+      });
       $scope.title = '';
+      $scope.link = '';
     };
     $scope.incrementUpvotes = function(post) {
       post.upvotes += 1;
