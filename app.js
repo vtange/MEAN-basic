6a680581e59c0f6366103ee360996708194a6331
Victor Tang <victortang1992@gmail.com>
2015-10-23 19:02:49 -0700

    detailed ngrepeat

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ var app = angular.module('flapperNews', []);
 
 app.controller('MainCtrl', ['$scope', function($scope){
   $scope.test = 'Hello world!';
-  $scope.posts = [
-  'post 1',
-  'post 2',
-  'post 3',
-  'post 4',
-  'post 5'
+    $scope.posts = [
+      {title: 'post 1', upvotes: 5},
+      {title: 'post 2', upvotes: 2},
+      {title: 'post 3', upvotes: 15},
+      {title: 'post 4', upvotes: 9},
+      {title: 'post 5', upvotes: 4}
     ];
 }]);
