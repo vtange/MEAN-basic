724175e7e31daf571c91237ea2dc56e32c3c4d9f
Victor Tang <victortang1992@gmail.com>
2015-10-23 21:36:01 -0700

    add new posts

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var app = angular.module('flapperNews', []);
 
 app.controller('MainCtrl', ['$scope', function($scope){
-  $scope.test = 'Hello world!';
+    $scope.test = 'Hello world!';
     $scope.posts = [
       {title: 'post 1', upvotes: 5},
       {title: 'post 2', upvotes: 2},
@@ -9,4 +9,9 @@ app.controller('MainCtrl', ['$scope', function($scope){
       {title: 'post 4', upvotes: 9},
       {title: 'post 5', upvotes: 4}
     ];
+    $scope.addPost = function(){
+      if(!$scope.title || $scope.title === '') { return; }
+      $scope.posts.push({title: $scope.title, upvotes: 0});
+      $scope.title = '';
+    };
 }]);
