f1749f66a81cc4cfef64cefc9229fe51f3b2ddb6
Victor Tang <victortang1992@gmail.com>
2015-10-23 18:30:29 -0700

    angular basic, repeat

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-app
+var app = angular.module('flapperNews', []);
+
+app.controller('MainCtrl', ['$scope', function($scope){
+  $scope.test = 'Hello world!';
+  $scope.posts = [
+  'post 1',
+  'post 2',
+  'post 3',
+  'post 4',
+  'post 5'
+    ];
+}]);
