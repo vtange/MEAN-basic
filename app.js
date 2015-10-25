4cf64e44084092d04d832a18b4a1a365bb69cc73
Victor Tang <victortang1992@gmail.com>
2015-10-24 18:41:19 -0700

    add mock posts, posts state

--- app.js
+++ app.js
@@ -10,17 +10,24 @@ function($stateProvider, $urlRouterProvider) {
       url: '/home',
       templateUrl: '/home.html',
       controller: 'MainCtrl'
+    })
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl'
     });
 
+
   $urlRouterProvider.otherwise('home');
-}]);
+}]);//end of config
 
 app.factory('posts', [function(){
   var o = {
     posts: []
   };
   return o;
-}]);
+}]);//end of service
+
 app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts){
     $scope.test = 'Hello world!';
     $scope.posts = posts.posts;
@@ -37,4 +44,22 @@ app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts){
     $scope.incrementUpvotes = function(post) {
       post.upvotes += 1;
     };
-}]);
+    //mock posts
+    $scope.posts.push({
+      title: $scope.title,
+      link: $scope.link,
+      upvotes: 0,
+      comments: [
+        {author: 'Joe', body: 'Cool post!', upvotes: 0},
+        {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+      ]
+    });
+}]);//end of controller
+
+app.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts){
+
+}]);//end of controller
