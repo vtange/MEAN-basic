2cfc8121091409875857bc4f669a38b71a7a14ce
Victor Tang <victortang1992@gmail.com>
2015-10-24 23:43:10 -0700

    comments postable, votable

--- app.js
+++ app.js
@@ -54,4 +54,14 @@ app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts){
 
 app.controller('PostsCtrl', ['$scope', '$stateParams', 'posts', function($scope, $stateParams, posts){
     $scope.post = posts.posts[$stateParams.id];
+    $scope.addComment = function(){
+      if($scope.body === '') { return; }
+      $scope.post.comments.push({
+        body: $scope.body,
+        author: 'user',
+        upvotes: 0
+      });
+      $scope.body = '';
+    };
+    
 }]);//end of controller
