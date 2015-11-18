8d7afbac9eb1fecfce5683684b5800b9d48d1c1a
Victor Tang <victortang1992@gmail.com>
2015-11-17 22:24:04 -0800

    add posting functions

--- nodeJS_version/flapper-news/routes/index.js
+++ nodeJS_version/flapper-news/routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+//loads comments and posts
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+//get posts
+router.get('/posts', function(req, res, next) {
+  Post.find(function(err, posts){
+    if(err){ return next(err); }
+
+    res.json(posts);
+  });
+});
+
+//make new post function
+router.post('/posts', function(req, res, next) {
+  var post = new Post(req.body);
+
+  post.save(function(err, post){
+    if(err){ return next(err); }
+
+    res.json(post);
+  });
 });
 
 module.exports = router;
